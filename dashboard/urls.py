b6497d47f62b2009372ca2a2eed6e55399ef949e
Mish-Meng <Michelle.Mengich@strathmore.edu>
2025-09-22 22:47:09 +0300

    Added home and about templates

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),       # http://127.0.0.1:8000/
+    path('about/', views.about, name='about'),  # http://127.0.0.1:8000/about/
 ]
